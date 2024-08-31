b6baf878ecfc7eafe2f78dad65e9b8ac2226b30a
Safeena Shaik <safeenashaik19@gmail.com>
2024-08-31 14:58:58 +0530

    Add files via upload

---
+++ roomtemp.ino
@@ -0,0 +1,15 @@
+int val=0;
+float mv=0,cel=0;
+void setup() {
+  pinMode(A5,INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  val = analogRead(A5);
+  Serial.println(val);
+  mv = (val/1024.0)*5000;
+  cel = mv/10;
+  Serial.println(cel);
+  delay(1000);
+}
